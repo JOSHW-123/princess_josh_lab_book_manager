7d90bedffa363abe6381374f54c990ba1c2caca4
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-08 11:23:20 +0100

    First Commit

---
+++ books_start_point/controllers/books_controller.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.book import Book
+
+books_blueprint = Blueprint("books", __name__)
+
+# NEW
+# GET '/books/new'
+Wohhoooooooo there's a ghost in your computer Joshuaaaaaaa
+
+# CREATE
+# POST '/books'
+
+# SHOW
+# GET '/books/<id>'
+
+# EDIT
+# GET '/books/<id>/edit'
+
+# UPDATE
+# PUT '/books/<id>'
+
+# DELETE
+# DELETE '/books/<id>'
